0c7e13d0699bc3fc78618f764f4dc2d2e16aa4d8
Amna <163533874+AmnaNaeem123@users.noreply.github.com>
2024-03-28 14:29:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// tea
+// 2 Sugar, 1 Tea, 1.5 Milk
+function tea(Sugar, Tea) {
+    console.log(Sugar + Tea + 1.5);
+}
+tea(3, 1.5); // raffy
+tea(3, 2); // me
+var order = tea(3, 1.5);
+console.log("order");
